4112b79dd6adbe4c0315b2f9e2b0504a65d8c26e
Sheema parveen <sheemaparveenfeb14@gmail.com>
2023-08-19 18:43:08 +0530

    all elements location are stored here

---
+++ Test_Location/location.py
@@ -0,0 +1,23 @@
+class Location:
+    make_appointment="btn-make-appointment"
+    username_inputbox="txt-username"
+    password_inputbox="txt-password"
+    login_button="btn-login"
+    menu_login="menu-toggle"
+    
+    login_button2='//*[@id="sidebar-wrapper"]/ul/li[3]/a'
+    facility_id="combo_facility"
+    apply_readmission="chk_hospotal_readmission"
+    medicaid="radio_program_medicaid"
+    visit_date="txt_visit_date"
+    comment_box="txt_comment"
+    book_appointment="btn-book-appointment"
+    gotohomepage='/html/body/section/div/div/div[7]/p/a'
+
+    menu="menu-toggle"
+    home="/html/body/nav/ul/li[2]/a"
+    history="/html/body/nav/ul/li[3]/a"
+    profile="/html/body/nav/ul/li[4]/a"
+    logout="/html/body/nav/ul/li[5]/a"
+    proflog="/html/body/section/div/div/div/p[2]/a"
+    
