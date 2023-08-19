2119192941cc983922070a08a127637d91141dae
Sheema parveen <sheemaparveenfeb14@gmail.com>
2023-08-19 18:41:46 +0530

    all values to fill the form are stored here

---
+++ Test_Information/info.py
@@ -0,0 +1,7 @@
+class Info:
+    url="https://katalon-demo-cura.herokuapp.com/"
+    appointmenturl="https://katalon-demo-cura.herokuapp.com/#appointment"
+    username="John Doe"
+    password="ThisIsNotAPassword"
+    visitdate="22/08/2023"
+    comment="thank you for giving appointment"
---
+++ Test_Information/invalid_info.py
@@ -0,0 +1,4 @@
+class Invalid_info:
+    url=url="https://katalon-demo-cura.herokuapp.com/"
+    invalid_username="sheema"
+    invalid_password="sheema14"
